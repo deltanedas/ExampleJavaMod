7ce8a7221e53477a5ae17f6a9b468ad46327e38e
DeltaNedas <deltanedas@gmail.com>
2021-02-05 03:06:08 +0000

    add fork script

---
+++ fork.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Usage: ./fork.sh MyUsername MyModName
+
+username=$1
+modname=$2
+
+# Replace release link + CI badge
+sed -i README.md -e "s/DeltaNedas/$username/g"
+# Replace mod name
+sed -i $(find . -type f) -e "s/ExampleJavaMod/$modname/g"
+# Rename main class (youll have to rename the package yourself)
+mv src/example/ExampleJavaMod.java "src/example/$modname.java"
+# Change remote for quick pushing
+git remote set-url origin "ssh://git@github.com/$username/$modname"
